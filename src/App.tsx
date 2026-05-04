e4bbf11b4e4733e1a00fc02db94af49a8b9101e3
crousty24-bit <allen.kch24@gmail.com>
2026-05-04 14:16:55 +0200

    feat: implement topbar search for filtering active projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AppShell } from "./components/AppShell";
 import { AppDataProvider } from "./features/app-data/AppDataProvider";
 import { BrowserRouter, Route, Routes } from "./lib/router";
@@ -7,13 +8,27 @@ import { HomePage } from "./pages/HomePage";
 import "./App.css";
 
 function App() {
+	const [projectSearchQuery, setProjectSearchQuery] = useState("");
+
 	return (
 		<AppDataProvider>
 			<BrowserRouter>
 				<Routes>
-					<Route element={<AppShell />}>
+					<Route
+						element={
+							<AppShell
+								projectSearchQuery={projectSearchQuery}
+								onProjectSearchChange={setProjectSearchQuery}
+							/>
+						}
+					>
 						<Route index element={<HomePage />} />
-						<Route path="/dashboard" element={<DashboardPage />} />
+						<Route
+							path="/dashboard"
+							element={
+								<DashboardPage projectSearchQuery={projectSearchQuery} />
+							}
+						/>
 						<Route path="/archives" element={<ArchivesPage />} />
 					</Route>
 				</Routes>
